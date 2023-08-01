8cc3f2f0cd8ce5d8363c82d4ea2192e542bf931b
948655206 <948655206@qq.com>
2023-08-01 20:35:43 +0800

    1

--- app/src/main/assets/chart/js/template.js
+++
@@ -1,9 +0,0 @@
-function loadChartView(id, option) {
-    var myChart = echarts.init(document.getElementById(id));
-    myChart.setOption(option);
-}
-
-function initChart() {
-    var option = JSON.parse(window.Android.makeBarChartOptions());
-    loadChartView('chart', option)
-}
